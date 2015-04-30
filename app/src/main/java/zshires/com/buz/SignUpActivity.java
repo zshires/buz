78e5dd43e2fa8441d7c248931406333e7cd305e5
Konnor <kbeaulier@wisc.edu>
2015-04-29 22:08:19 -0500

    Finish sign up function

--- app/src/main/java/zshires/com/buz/SignUpActivity.java
+++ app/src/main/java/zshires/com/buz/SignUpActivity.java
@@ -12,8 +12,10 @@ import android.widget.Toast;
 import android.content.Intent;
 
 import org.apache.http.Header;
+import org.apache.http.NameValuePair;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
 
 import net.callumtaylor.asynchttp.AsyncHttpClient;
@@ -63,15 +65,18 @@ public class SignUpActivity extends ActionBarActivity {
 
         AsyncHttpClient client = new AsyncHttpClient(url);
         StringEntity jsonParams = null;
+        List<NameValuePair> params = null;
 
         try{
-            JSONObject json = new JSONObject();
-            json.put("username", username);
-            json.put("password", null);
-            json.put("latitude", null);
-            json.put("longitude", null);
-            jsonParams = new StringEntity(json.toString());
-            Log.d("jsonParam", jsonParams.toString());
+            //JSONObject json = new JSONObject();
+            params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("username", username));
+            //json.put("username", username);
+            //json.put("phonenumber", "2629021681");
+            //json.put("latitude", "333");
+            //json.put("longitude", "444");
+            //jsonParams = new StringEntity(json.toString());
+            //Log.d(TAG, json.toString());
 
         }catch(Exception e){
             e.printStackTrace();
@@ -81,9 +86,9 @@ public class SignUpActivity extends ActionBarActivity {
         headers.add(new BasicHeader("Accept", "application/json"));
         headers.add(new BasicHeader("Content-Type", "application/json"));
 
-        Log.d("url intent", url + "users.json");
+        //client.post("users.json", jsonParams, headers,new JsonResponseHandler() {
 
-        client.post("users.json", jsonParams, headers, new JsonResponseHandler() {
+        client.post("users.json", params, headers,new JsonResponseHandler() {
             @Override
             public void onSuccess() {
                 runOnUiThread(new Runnable() {
