5b0ed4481b82260e40854fe31b3144de5f7e2741
Zak Shires <zshires@yahoo.com>
2015-05-06 14:32:55 -0500

    added notifications

--- app/src/main/java/zshires/com/buz/MessagesActivity.java
+++ app/src/main/java/zshires/com/buz/MessagesActivity.java
@@ -1,9 +1,14 @@
 package zshires.com.buz;
 
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -15,6 +20,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.GregorianCalendar;
 
 
 public class MessagesActivity extends ActionBarActivity {
@@ -28,6 +35,17 @@ public class MessagesActivity extends ActionBarActivity {
     // thread
     Handler mHandler = new Handler();
 
+    Button showNotificationBut, stopNotificationBut, alertButton;
+
+    // Allows us to notify the user that something happened in the background
+    NotificationManager notificationManager;
+
+    // Used to track notifications
+    int notifID = 33;
+
+    // Used to track if notification is active in the task bar
+    boolean isNotificActive = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +55,11 @@ public class MessagesActivity extends ActionBarActivity {
         pNumEditText = (EditText) findViewById(R.id.pNumEditText);
         messagesEditText = (EditText) findViewById(R.id.messagesEditText);
         sendButton = (Button) findViewById(R.id.sendButton);
+
+        // Initialize notificaiton buttons
+        showNotificationBut = (Button) findViewById(R.id.showNotificationBut);
+        stopNotificationBut = (Button) findViewById(R.id.stopNotificationBut);
+        alertButton = (Button) findViewById(R.id.alertButton);
 
         // Thread updates the messages EditText every 10 seconds
         new Thread(new Runnable() {
@@ -219,5 +242,77 @@ public class MessagesActivity extends ActionBarActivity {
     public void openSettings() {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
+    }
+
+    public void showNotification(View view) {
+
+        // Builds a notification
+        NotificationCompat.Builder notificBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle("Message")
+                .setContentText("New Message")
+                .setTicker("Alert New Message")
+                .setSmallIcon(R.drawable.bee);
+
+        // Define that we have the intention of opening MoreInfoNotification
+        Intent moreInfoIntent = new Intent(this, MoreInfoNotification.class);
+
+        // Used to stack tasks across activites so we go to the proper place when back is clicked
+        TaskStackBuilder tStackBuilder = TaskStackBuilder.create(this);
+
+        // Add all parents of this activity to the stack
+        tStackBuilder.addParentStack(MoreInfoNotification.class);
+
+        // Add our new Intent to the stack
+        tStackBuilder.addNextIntent(moreInfoIntent);
+
+        // Define an Intent and an action to perform with it by another application
+        // FLAG_UPDATE_CURRENT : If the intent exists keep it but update it if needed
+        PendingIntent pendingIntent = tStackBuilder.getPendingIntent(0,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Defines the Intent to fire when the notification is clicked
+        notificBuilder.setContentIntent(pendingIntent);
+
+        // Gets a NotificationManager which is used to notify the user of the background event
+        notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Post the notification
+        notificationManager.notify(notifID, notificBuilder.build());
+
+        // Used so that we can't stop a notification that has already been stopped
+        isNotificActive = true;
+
+
+    }
+
+    public void stopNotification(View view) {
+
+        // If the notification is still active close it
+        if(isNotificActive) {
+            notificationManager.cancel(notifID);
+        }
+
+    }
+
+    public void setAlarm(View view) {
+
+        // Define a time value of 5 seconds
+        Long alertTime = new GregorianCalendar().getTimeInMillis()+5*1000;
+
+        // Define our intention of executing AlertReceiver
+        Intent alertIntent = new Intent(this, AlertReceiver.class);
+
+        // Allows you to schedule for your application to do something at a later date
+        // even if it is in he background or isn't active
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        // set() schedules an alarm to trigger
+        // Trigger for alertIntent to fire in 5 seconds
+        // FLAG_UPDATE_CURRENT : Update the Intent if active
+        alarmManager.set(AlarmManager.RTC_WAKEUP, alertTime,
+                PendingIntent.getBroadcast(this, 1, alertIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT));
+
     }
 }
---
+++ app/src/main/java/zshires/com/buz/MoreInfoNotification.java
@@ -0,0 +1,14 @@
+package zshires.com.buz;
+
+        import android.app.Activity;
+        import android.os.Bundle;
+
+// Called when the notification is clicked on in the task bar
+public class MoreInfoNotification extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.more_info_notific);
+    }
+}
